b4d27251a137226092a6cdb56a471de293939343
Samuel Domínguez Lorenzo <samueldominguez@protonmail.com>
2019-11-14 16:26:09 +0000

    part of Sprint 3, first commit

---
+++ NewsSpotify/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace NewsSpotify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Terms: (AND between terms)";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 57);
+            this.textBox1.MaxLength = 30;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Maximum of Tracks Returned:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(42, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Use News Headlines?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate Tracks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Excluded Sources:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(224, 48);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(182, 104);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(224, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "(one per line)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 168);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NewsSpotify Application UX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NewsSpotify/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewsSpotify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // default selection on combobox is the first item, which is 1
+            comboBox1.SelectedIndex = 0;
+            // the search terms text box is enabled by default
+            textBox1.Enabled = true;
+            // the use news checbox is unticked by default
+            checkBox1.Checked = false;
+            // the excluded sources text box is disabled by default
+            textBox2.Enabled = false;
+            // do not allow users to resize the form
+            MaximizeBox = false;
+            MinimizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                // disable search terms text box
+                textBox1.Enabled = false;
+                // enable excluded terms text box
+                textBox2.Enabled = true;
+            } else
+            {
+                // enable search terms text box
+                textBox1.Enabled = true;
+                // disable excluded sources text box
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // we are about to generate tracks
+            // get the maximum number of tracks to output
+            // note: due to the nature of the combobox, we know
+            // for sure that the value of it is an integer, since we
+            // have defined the values that go in there ourselves
+            String max_track_out = comboBox1.Text;
+            // this is to execute the process
+            ProcessStartInfo start_info = new ProcessStartInfo();
+            start_info.FileName = "ns_main\\ns_main.exe";
+            start_info.Arguments = "";
+            start_info.UseShellExecute = false;
+            start_info.RedirectStandardOutput = true;
+            start_info.CreateNoWindow = true;
+            start_info.WorkingDirectory = "ns_main";
+            Process python_proc = new Process();
+
+            // add the max tracks option to the arguments
+            start_info.Arguments += "--max=" + max_track_out + " ";
+
+            // check which type of query we are doing
+            if (checkBox1.Checked)
+            {
+                // we are using news headlines
+                // collect excluded sources
+
+                // add the news options to the argument line
+                start_info.Arguments += "--news ";
+
+                var excl_sources = new List<String>();
+                // add those defined sources which are not whitespace or empty
+                foreach (String line in textBox2.Lines)
+                {
+                    if (!String.IsNullOrWhiteSpace(line)) excl_sources.Add(line);
+                }
+                // now we build the command and run it
+                // DEBUG!
+                Debug.WriteLine("Excluded Sources:");
+                foreach (String line in excl_sources)
+                {
+                    Debug.WriteLine(line);
+                }
+                Debug.WriteLine("End of excluded sources.");
+                Debug.WriteLine("Max Tracks: ", max_track_out);
+                // write excluded sources to a file in ns_main directory
+                System.IO.File.WriteAllLines(@"ns_main\.exclusion", excl_sources.ToArray());
+                // add this to the arguments
+                start_info.Arguments += " --exclude=.exclusion";
+                // set the process information before running it
+                python_proc.StartInfo = start_info;
+                // run the process
+                python_proc.Start();
+                while (!python_proc.StandardOutput.EndOfStream)
+                {
+                    string line = python_proc.StandardOutput.ReadLine();
+                    Debug.WriteLine(line);
+                }
+                python_proc.WaitForExit();
+                // DEBUG!
+            } else
+            {
+                // we are using terms input by the user
+                // collect the terms
+                // set the delimiter chars
+                char[] delimiters = {' ', '-', ',', '.', ':', '!', '?', ';'};
+                String[] search_terms = textBox1.Text.Split(delimiters);
+                var clean_search_terms = new List<String>();
+                foreach (String term in search_terms)
+                {
+                    if (!String.IsNullOrWhiteSpace(term)) clean_search_terms.Add(term);
+                }
+                // check there are no empty search terms
+                if (clean_search_terms.Count < 1)
+                {
+                    // we cannot continue without search terms,
+                    // tell the user
+                    MessageBox.Show("You have not provided a valid amount of search terms. Cannot output tracks.",
+                        "Fatal Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    // exit
+                    return;
+                } else
+                {
+                    // otherwise, add the search terms to the arguments
+                    foreach (string term in clean_search_terms)
+                    {
+                        start_info.Arguments += " " + term;
+                    }
+                }
+                // DEBUG!
+                Debug.WriteLine("Search Terms:");
+                foreach (String line in clean_search_terms)
+                {
+                    Debug.WriteLine(line);
+                }
+                Debug.WriteLine("End of search terms.");
+                Debug.WriteLine("Max Tracks: ", max_track_out);
+                python_proc.StartInfo = start_info;
+                python_proc.Start();
+                while (!python_proc.StandardOutput.EndOfStream)
+                {
+                    string line = python_proc.StandardOutput.ReadLine();
+                    Debug.WriteLine(line);
+                }
+                python_proc.WaitForExit();
+                // DEBUG!
+            }
+        }
+    }
+}
